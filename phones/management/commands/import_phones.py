4a349740b920f3dd996bb741cef3d8afc1b49d69
iragordionok <irina.gordionok@yandex.ru>
2024-03-05 18:31:23 +0300

    Initial commit

---
+++ phones/management/commands/import_phones.py
@@ -0,0 +1,24 @@
+import csv
+from django.utils.text import slugify
+
+from django.core.management.base import BaseCommand
+from phones.models import Phone
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        with open('phones.csv', 'r') as csvfile:
+            phones = list(csv.DictReader(csvfile, delimiter=';'))
+
+            for phone in phones:
+                Phone.objects.create(
+                    id=phone['id'],
+                    name=phone['name'],
+                    price=phone['price'],
+                    image=phone['image'],
+                    release_date=phone['release_date'],
+                    lte_exists=phone['lte_exists'],
+                    slug=slugify(phone['name']))
---
+++ phones/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from phones.models import Phone
+
+
+def show_catalog(request):
+    template = 'catalog.html'
+    sort_pages = request.GET.get('sort', '')
+    all_phones = Phone.objects.all()
+
+    if sort_pages == 'max_price':
+        phones = all_phones.order_by('-price')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    elif sort_pages == 'min_price':
+        phones = all_phones.order_by('price')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    elif sort_pages == 'name':
+        phones = all_phones.order_by('name')
+        context = {'phones': phones}
+        return render(request, template, context)
+
+    context = {'phones': all_phones}
+    return render(request, template, context)
+
+
+def show_product(request, slug):
+    template = 'product.html'
+    phones = Phone.objects.get(slug=slug)
+    context = {'phones': phones}
+    return render(request, template, context)
+
